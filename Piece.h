f8da690a09d29530f20a8a6effd812c85f2a30e5
hadiyaaamer22 <hadiyaaamer22@gmail.com>
2026-05-09 22:55:52 +0500

    Added rook and knight movement

--- Piece.h
+++ Piece.h
@@ -46,3 +46,47 @@ public:
         return false;
     }
 };
+class Rook:public Piece
+{
+public:
+    Rook(char c):Piece(c,c=='W'?'R':'r'){}
+
+    bool isValidMove(int fr,int fc,int tr,int tc,Piece* board[8][8])const override
+    {
+        if(fr!=tr&&fc!=tc)
+            return false;
+
+        int rs=(tr>fr)?1:(tr<fr)?-1:0;
+        int cs=(tc>fc)?1:(tc<fc)?-1:0;
+        int r=fr+rs;
+        int c=fc+cs;
+
+        while(r!=tr||c!=tc)
+        {
+            if(board[r][c]!=nullptr)
+                return false;
+
+            r+=rs;
+            c+=cs;
+        }
+
+        return board[tr][tc]==nullptr||board[tr][tc]->getColor()!=color;
+    }
+};
+
+class Knight:public Piece
+{
+public:
+    Knight(char c):Piece(c,c=='W'?'N':'n'){}
+
+    bool isValidMove(int fr,int fc,int tr,int tc,Piece* board[8][8])const override
+    {
+        int dr=abs(tr-fr);
+        int dc=abs(tc-fc);
+
+        if(!((dr==2&&dc==1)||(dr==1&&dc==2)))
+            return false;
+
+        return board[tr][tc]==nullptr||board[tr][tc]->getColor()!=color;
+    }
+};

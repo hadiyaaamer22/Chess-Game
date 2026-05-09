bfdc4f147f0d4f524677f7cb8a7685cad35c4ecc
hadiyaaamer22 <hadiyaaamer22@gmail.com>
2026-05-09 23:19:28 +0500

    Added libraries and input parser

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include "Board.h"
 using namespace std;
@@ -17,49 +17,4 @@ bool parseSquare(const string& s, int& row, int& col) {
     row = 8 - (rank - '0');
 
     return true;
-}
-
-// Entry point
-int main() {
-    cout << "========================================\n";
-    cout << "        CHESS GAME         \n";
-    cout << "========================================\n";
-    cout << "Pieces: K/k=King  Q/q=Queen  R/r=Rook\n";
-    cout << "        B/b=Bishop  N/n=Knight  P/p=Pawn\n";
-    cout << "UPPERCASE = White     lowercase = Black\n";
-    cout << "Enter moves like: e2 e4\n";
-    cout << "Type 'quit' to exit.\n";
-
-    Board board;
-    char turn = 'W';   // White goes first
-
-    while (true) {
-        board.display();
-
-        cout << (turn == 'W' ? "White" : "Black") << "'s turn.\n";
-        cout << "Enter move (from to): ";
-
-        string from, to;
-        cin >> from;
-
-        if (from == "quit") {
-            cout << "Game ended by player.\n";
-            break;
-        }
-
-        cin >> to;
-
-        int fr, fc, tr, tc;
-
-        if (!parseSquare(from, fr, fc) || !parseSquare(to, tr, tc)) {
-            cout << "Bad input! Use format like 'e2 e4'.\n";
-            continue;
-        }
-
-        if (board.movePiece(fr, fc, tr, tc, turn)) {
-            turn = (turn == 'W') ? 'B' : 'W';  // switch turn
-        }
-    }
-
-    return 0;
 }
